fcee78c1238a65076d30fe0d411b229297dbc6c9
BuildTools <unconfigured@null.spigotmc.org>
2020-09-16 11:05:44 -0400

    Commit by Cam on September 16, 2020 11:05:44

--- Part 2 - Echo/echo.py
+++ Part 2 - Echo/echo.py
@@ -1,5 +1,5 @@
-# Title: EE 321 Project 1: Audio Manipulation. Part I: Delay
-# Purpose: To implement a delay into a .wav audio file
+# Title: EE 321 Project 1: Audio Manipulation. Part II: Echo
+# Purpose: To implement echoes into a .wav audio file
 # Developers: Cameron Palmer, Shawn Boyd, Siddesh Sood
 # Last Modified: September 15, 2020
 
@@ -26,57 +26,54 @@ print("Length: " + str(round(TOTALFRAMES/SAMPLERATE, 2)) + "s")
 print("Sample width: " + str(SAMPLEWIDTH) + " bytes, or " + str(SAMPLEWIDTH*8) + " bits")
 print("////////////////////////////////////////////////////////////////\n")
 
-# Open the wave files we'll create to store the original audio file with different delays
-audioOut0 = wave.open("delay0.wav", "w")
+# Open the wave files we'll create to store the original audio file with different echoes
+audioOut0 = wave.open("echo0.wav", "w")
 audioOut0.setnchannels(2)
 audioOut0.setsampwidth(2)
 audioOut0.setframerate(44100)
 
-audioOut1 = wave.open("delay1.wav", "w")
+audioOut1 = wave.open("echo1.wav", "w")
 audioOut1.setnchannels(2)
 audioOut1.setsampwidth(2)
 audioOut1.setframerate(44100)
 
-audioOut2 = wave.open("delay2.wav", "w")
+audioOut2 = wave.open("echo2.wav", "w")
 audioOut2.setnchannels(2)
 audioOut2.setsampwidth(2)
 audioOut2.setframerate(44100)
-
 
 
 # Get the raw audio from the wave file and convert it to an ndarray
 signal = audio.readframes(-1)
 signal = np.frombuffer(signal, "Int16", -1)
 
-# Create the variable signalarray of type bytearray so we can add a delay in the front of it
+# Create a variable signalarray of type bytearray. This now contains the original audio
 signalarray = bytearray(signal)
 
-# Create the delays of type bytearray
+# Create a one second delay to place between echoes
 zerosarray1sec = bytearray(BYTESINASEC)
-zerosarray3sec = bytearray(BYTESINASEC*3)
-zerosarray5sec = bytearray(BYTESINASEC*5)
+#print(signalarray)
 
-# Add in the delays and write out the delayed files
-signalOut0 = zerosarray1sec + signalarray
+# Create some divisor bytearrays to create the echoes with
+echodivisor0 = [4] * len(signal)
+
+# Create some copies of the original audio with lower gain
+#echoinstanceint0 = [int(v) for v in signalarray.split()]
+#print(echoinstanceint0)
+echoinstance0 = signal / echodivisor0
+echoinstance0 = bytearray(echoinstance0)
+
+# Create our echoed audio
+signalOut0 = signalarray + zerosarray1sec
+signalOut0[-1:-1] = echoinstance0
 signalOut0 = np.frombuffer(signalOut0, "Int16", -1)
 audioOut0.writeframes(signalOut0)
-
-signalOut1 = zerosarray3sec + signalarray
-signalOut1 = np.frombuffer(signalOut1, "Int16", -1)
-audioOut1.writeframes(signalOut1)
-
-signalOut2 = zerosarray5sec + signalarray
-signalOut2 = np.frombuffer(signalOut2, "Int16", -1)
-audioOut2.writeframes(signalOut2)
 
 
 ## Graphs
 
 # The time variables for the horizontal axis of the audios
 Time = np.linspace(0, len(signal)/SAMPLERATE, num=len(signal))
-Time0 = np.linspace(0, len(signalOut0)/SAMPLERATE, num=len(signalOut0))
-Time1 = np.linspace(0, len(signalOut1)/SAMPLERATE, num=len(signalOut1))
-Time2 = np.linspace(0, len(signalOut2)/SAMPLERATE, num=len(signalOut2))
 
 # Graph of original audio
 plt.figure(1)
@@ -86,26 +83,7 @@ plt.xlabel("Time (s)")
 plt.ylabel("Value")
 plt.show()
 
-# Graph of 1st delayed audio
 plt.figure(2)
-plt.title("Audio with 1s delay")
-plt.plot(Time0/NUMCHANNELS, signalOut0/NUMCHANNELS)
-plt.xlabel("Time (s)")
-plt.ylabel("Value")
-plt.show()
-
-# Graph of 2nd delayed audio
-plt.figure(3)
-plt.title("Audio with 3s delay")
-plt.plot(Time1/NUMCHANNELS, signalOut1/NUMCHANNELS)
-plt.xlabel("Time (s)")
-plt.ylabel("Value")
-plt.show()
-
-# Graph of 3rd delayed audio
-plt.figure(4)
-plt.title("Audio with 5s delay")
-plt.plot(Time2/NUMCHANNELS, signalOut2/NUMCHANNELS)
-plt.xlabel("Time (s)")
-plt.ylabel("Value")
+plt.title("Echo 1")
+plt.plot(np.frombuffer(signalarray, "Int16", -1))
 plt.show()
