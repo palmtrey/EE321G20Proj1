5024a2fae1558fdd10d203ccfe6c85167e9cec4a
BuildTools <unconfigured@null.spigotmc.org>
2020-09-17 11:33:42 -0400

    Commit by Cam on September 17, 2020 11:33:42

--- Part 3 - Reverberation/reverb.py
+++ Part 3 - Reverberation/reverb.py
@@ -1,7 +1,7 @@
 # Title: EE 321 Project 1: Audio Manipulation. Part II: Echo
 # Purpose: To implement echoes into a .wav audio file
 # Developers: Cameron Palmer, Shawn Boyd, Siddesh Sood
-# Last Modified: September 16, 2020
+# Last Modified: September 17, 2020
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -42,17 +42,12 @@ audioOut2.setnchannels(2)
 audioOut2.setsampwidth(2)
 audioOut2.setframerate(44100)
 
-
 # Get the raw audio from the wave file and convert it to an ndarray
 signal = audio.readframes(-1)
 signal = np.frombuffer(signal, np.int16, -1)
 
 # Create a variable signalarray of type bytearray. This now contains the original audio
 signalarray = bytearray(signal)
-
-# Create a one second delay to place between echoes
-zerosarray1sec = bytearray(BYTESINASEC)
-zerosarray1sec = np.frombuffer(zerosarray1sec, np.int16, -1)
 
 # Convert signalarray back into an ndarray to be able to write it out
 signalOut = np.frombuffer(signalarray, np.int16, -1)
@@ -67,29 +62,36 @@ echo1 = echo1.astype(np.int16)
 echo2 = np.round(np.frombuffer(signalOut, np.int16, -1)*0.5)
 echo2 = echo2.astype(np.int16)
 
-print(signalOut[len(signalOut)-50000-1:-1].shape)
-print(echo0[0:50000].shape)
-
+# Create a constant to standardize how much the reverberations overlap
+AMOUNTTOREVERB = 100000
 
 # Creating the first audio file with reverberations (1 reverberation) #
-signalOut0 = np.add(signalOut[len(signalOut)-50000-1:-1], echo0[0:50000])
-signalOut0 = np.append(signalOut, echo0[50000:-1])
+signalOut0 = signalOut[0:len(signalOut)-AMOUNTTOREVERB-1]  # Create the non-overlapped part of the signal
+signalOut0 = np.append(signalOut0, np.add(signalOut[len(signalOut)-AMOUNTTOREVERB-1:-1], echo0[0:AMOUNTTOREVERB]))  # Add in the overlapped part of the signal
+signalOut0 = np.append(signalOut0, echo0[AMOUNTTOREVERB:-1])  # Finish with the non-overlapped end of the echo
 
 audioOut0.writeframes(signalOut0)
 
+# Creating the second audio file with reverberations (2 reverberations)
+signalOut1 = signalOut0[0:len(signalOut0)-AMOUNTTOREVERB-1]  # Start with the previous reverberated signal
+signalOut1 = np.append(signalOut1, np.add(signalOut0[len(signalOut0)-AMOUNTTOREVERB-1:-1], echo1[0:AMOUNTTOREVERB]))
+signalOut1 = np.append(signalOut1, echo1[AMOUNTTOREVERB:-1])
 
+audioOut1.writeframes(signalOut1)
 
+# Creating the third audio file with reverberations
+signalOut2 = signalOut1[0:len(signalOut1)-AMOUNTTOREVERB-1]
+signalOut2 = np.append(signalOut2, np.add(signalOut1[len(signalOut1)-AMOUNTTOREVERB-1:-1], echo2[0:AMOUNTTOREVERB]))
+signalOut2 = np.append(signalOut2, echo2[AMOUNTTOREVERB:-1])
 
+audioOut2.writeframes(signalOut2)
 
 # Close all the audio streams
 audioOut0.close()
 audioOut1.close()
 audioOut2.close()
+
 ## Graphs
-
-
-
-
 # Graph of original audio
 Time = np.linspace(0, len(signal)/SAMPLERATE, num=len(signal))  # For horizontal axis
 plt.figure(1)
@@ -97,7 +99,6 @@ plt.title("Original Audio")
 plt.plot(Time/NUMCHANNELS, signal/NUMCHANNELS)
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
-plt.show()
 
 # Graph of first echo file (1 echo)
 audioGraph0 = wave.open("reverb0.wav")
@@ -106,11 +107,10 @@ signalGraph0 = np.frombuffer(signalGraph0, np.int16, -1)
 
 Time0 = np.linspace(0, len(signalGraph0)/SAMPLERATE, num=len(signalGraph0))  # For horizontal axis
 plt.figure(2)
-plt.title("Echo 1")
+plt.title("Reverberation 1")
 plt.plot(Time0/NUMCHANNELS, signalGraph0/NUMCHANNELS)
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
-plt.show()
 
 # Graph of second echo file (2 echoes)
 audioGraph1 = wave.open("reverb1.wav")
@@ -119,11 +119,10 @@ signalGraph1 = np.frombuffer(signalGraph1, np.int16, -1)
 
 Time1 = np.linspace(0, len(signalGraph1)/SAMPLERATE, num=len(signalGraph1))  # For horizontal axis
 plt.figure(3)
-plt.title("Echo 2")
+plt.title("Reverberation 2")
 plt.plot(Time1/NUMCHANNELS, signalGraph1/NUMCHANNELS)
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
-plt.show()
 
 # Graph of the third echo file (3 echoes)
 audioGraph2 = wave.open("reverb2.wav")
@@ -132,7 +131,7 @@ signalGraph2 = np.frombuffer(signalGraph2, np.int16, -1)
 
 Time2 = np.linspace(0, len(signalGraph2)/SAMPLERATE, num=len(signalGraph2))  # For horizontal axis
 plt.figure(4)
-plt.title("Echo 3")
+plt.title("Reverberation 3")
 plt.plot(Time2/NUMCHANNELS, signalGraph2/NUMCHANNELS)
 plt.xlabel("Time (s)")
 plt.ylabel("Value")
